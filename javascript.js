36e9378a85ce29dc4786e826e5f03cdb0ac2feef
josemanuelgarcia <josececos@gmail.com>
2016-03-30 16:55:12 +0200

    Añadidos bloques
    primera prueba generar codigo

--- javascript.js
+++ javascript.js
@@ -2,22 +2,22 @@ Blockly.JavaScript['bloque_principal'] = function(block) {
   var statements_variables = Blockly.JavaScript.statementToCode(block, 'variables');
   var statements_cuerpo = Blockly.JavaScript.statementToCode(block, 'cuerpo');
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = statements_variables +'<br/>'+ statements_cuerpo;
   return code;
 };
 
 Blockly.JavaScript['parar'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = 'parar_todo();<br/>';
   // TODO: Change ORDER_NONE to the correct strength.
-  return null;
+  return code;
 };
 
 Blockly.JavaScript['ir_a'] = function(block) {
   var text_coordenadax = block.getFieldValue('coordenadaX');
   var text_coordenaday = block.getFieldValue('coordenadaY');
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = 'ir_a('+text_coordenadax+','+text_coordenaday+');<br/>';
   return code;
 };
 
@@ -70,26 +70,8 @@ Blockly.JavaScript['cerrar_pinza'] = function(block) {
   return code;
 };
 
-Blockly.JavaScript['bucle_if'] = function(block) {
-  var value_condicion = Blockly.JavaScript.valueToCode(block, 'condicion', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_argumentos = Blockly.JavaScript.statementToCode(block, 'argumentos');
+Blockly.JavaScript['math_number'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.JavaScript['bucle_for'] = function(block) {
-  var statements_argumentos = Blockly.JavaScript.statementToCode(block, 'argumentos');
-  var text_numeroveces = block.getFieldValue('numeroVeces');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.JavaScript['bucle_while'] = function(block) {
-  var value_condicion = Blockly.JavaScript.valueToCode(block, 'condicion', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_argumentos = Blockly.JavaScript.statementToCode(block, 'argumentos');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = Blockly.JavaScript.statementToCode(block, 'NUM');
   return code;
 };
