93e91f720904ffcfa6988993d34194146a93a73a
josemanuelgarcia <josececos@gmail.com>
2016-07-08 12:35:37 +0200

    Modificada letra

--- public/bloques/posicionamiento.js
+++ public/bloques/posicionamiento.js
@@ -1,56 +1,56 @@
-Blockly.Blocks['establecer_posicion_inicial'] = {
-  init: function() {
-      this.jsonInit({
-      "message0": Blockly.Msg.POS_INICIAL,
-      "args0": [
-        {
-          "type": "field_input",
-          "name": "coordenadaX",
-          "text": "0"
-        },
-        {
-          "type": "field_input",
-          "name": "coordenadaY",
-          "text": "0"
-        }
-      ]
-    });    
+  Blockly.Blocks['establecer_posicion_inicial'] = {
+    init: function() {
+        this.jsonInit({
+        "message0": Blockly.Msg.POS_INICIAL,
+        "args0": [
+          {
+            "type": "field_input",
+            "name": "coordenadaX",
+            "text": "0"
+          },
+          {
+            "type": "field_input",
+            "name": "coordenadaY",
+            "text": "0"
+          }
+        ]
+      });    
 
-    this.getField('coordenadaX').setValidator(
-        Blockly.FieldTextInput.nonnegativeIntegerValidator);
-    this.getField('coordenadaY').setValidator(
-        Blockly.FieldTextInput.nonnegativeIntegerValidator);  
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(280);
-  }
-};
+      this.getField('coordenadaX').setValidator(
+          Blockly.FieldTextInput.nonnegativeIntegerValidator);
+      this.getField('coordenadaY').setValidator(
+          Blockly.FieldTextInput.nonnegativeIntegerValidator);  
+      this.setPreviousStatement(true);
+      this.setNextStatement(true);
+      this.setColour(280);
+    }
+  };
 
-Blockly.Blocks['establecer_tamanio_tablero'] = {
-  init: function() {
-      this.jsonInit({
-      "message0": Blockly.Msg.TAM_TABLERO,
-      "args0": [
-        {
-          "type": "field_input",
-          "name": "tamX",
-          "text": "3"
-        },
-        {
-          "type": "field_input",
-          "name": "tamY",
-          "text": "3"
-        }
-      ]
-    });    
+  Blockly.Blocks['establecer_tamanio_tablero'] = {
+    init: function() {
+        this.jsonInit({
+        "message0": Blockly.Msg.TAM_TABLERO,
+        "args0": [
+          {
+            "type": "field_input",
+            "name": "tamX",
+            "text": "3"
+          },
+          {
+            "type": "field_input",
+            "name": "tamY",
+            "text": "3"
+          }
+        ]
+      });    
 
-    this.getField('tamX').setValidator(
-        Blockly.FieldTextInput.nonnegativeIntegerValidator);
-    this.getField('tamY').setValidator(
-        Blockly.FieldTextInput.nonnegativeIntegerValidator);   
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(280);
-  }
-};
+      this.getField('tamX').setValidator(
+          Blockly.FieldTextInput.nonnegativeIntegerValidator);
+      this.getField('tamY').setValidator(
+          Blockly.FieldTextInput.nonnegativeIntegerValidator);   
+      this.setPreviousStatement(true);
+      this.setNextStatement(true);
+      this.setColour(280);
+    }
+  };
 
